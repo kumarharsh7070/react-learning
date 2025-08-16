04d7321f2cf7c55840e9f1cf40c3f5da0e152464
kumar harsh <hk995814@gmail.com>
2025-08-16 23:16:32 +0530

    custom react

---
+++ 01basic/src/App.jsx
@@ -0,0 +1,13 @@
+
+import First from "./First";
+
+function App() {
+  return(
+<>
+<First/>
+<p>test para</p>
+</>
+  );
+}
+
+export default App
---
+++ 01basic/src/First.jsx
@@ -0,0 +1,10 @@
+
+
+function First() {
+    return(
+        <h3>hello harry how are you</h3>
+    )
+
+}
+
+export default First
