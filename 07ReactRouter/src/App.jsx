dc7d3844ce69ee4741ca8c95ec2350d90ad0edb8
kumar harsh <hk995814@gmail.com>
2025-09-23 23:03:48 +0530

    routeing and components

--- 07ReactRouter/src/App.jsx
+++ 07ReactRouter/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="bg-green-400 p-4">Hello Harry</h1>
-    </>
+    
+     </>
   )
 }
 
--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/contact.jsx'
+import User from './components/User/User.jsx'
+const router= createBrowserRouter([
+  {
+    path: '/',
+    element:<Layout/>,
+    children:[
+      {path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About />
+      },
+      {
+        path:"contact",
+        element:<Contact />
+      },
+      {
+         path:"User/:userId",
+          element:<User />
+      }
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
