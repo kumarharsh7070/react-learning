0825df92f077bc70f9949c57ce1400b75467b7a8
kumar harsh <hk995814@gmail.com>
2025-09-27 23:57:46 +0530

    new project

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/*theme buttons*/}
+                    </div>
+qwdfg
+                    <div className="w-full max-w-sm mx-auto">
+                          {/*theme buttons*/}
+                    </div>
+                </div>
+            </div>
+
+    </>
+  )
+}
+
+export default App
