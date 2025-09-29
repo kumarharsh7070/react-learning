69eb2b5a1b21ea86bbb34e33012fb0c670ebd327
kumar harsh <hk995814@gmail.com>
2025-09-30 00:02:13 +0530

    on project

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 
 import './App.css'
+import { ThemeContext } from './context/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
 
+  const darkTheme = () => {
+    setThemeMode("dark")
+    document.documentElement.classList.add("dark")
+  }
+
+  const lightTheme = () => {
+    setThemeMode("light")
+    document.documentElement.classList.remove("dark")
+  }
   return (
-    <>
-     
-     
-<div className="flex flex-wrap min-h-screen items-center">
+
+     <ThemeContextProvider value={{themeMode, darkTheme, lightTheme}}>
+         <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                         {/*theme buttons*/}
@@ -20,8 +29,9 @@ qwdfg
                     </div>
                 </div>
             </div>
-
-    </>
+</ThemeContextProvider>
+     
+    
   )
 }
 
